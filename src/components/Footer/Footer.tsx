db3d553516847b3f9a3ae8f90f6f1689896dcddf
Shobhit Goel <itsshobhitgoel@gmail.com>
2024-08-16 20:30:36 +0530

    fix: lint issue

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -17,7 +17,6 @@ export const Footer = () => {
         </Container>
       </div>
 
-    
       <ItemsContainer />
       <div
         className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10
